b84e37872ce7fe8b9537908e3f7b153999f0de4d
Jaeger <kartoffel1014@163.com>
2017-05-21 02:09:06 +0800

    Initial project

---
+++ src/main/java/com/jaeger/SpringcloudApplication.java
@@ -0,0 +1,11 @@
+package com.jaeger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudApplication.class, args);
+    }
+}
